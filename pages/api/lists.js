5c2a1d961a98a7e1cc3366ed6d6fbb096ca7260b
Joao Braganca <joao.braganca@deus.ai>
2022-12-04 17:01:33 +0000

    testing endpoints and mongodb connection

---
+++ pages/api/lists.js
@@ -0,0 +1,18 @@
+import clientPromise from "../../lib/mongodb";
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db(process.env.MONGODB_DB);
+
+  switch (req.method) {
+    case "POST":
+      let bodyObject = JSON.parse(req.body);
+      let myList = await db.collection("Lists").insertOne(bodyObject);
+      res.json(myList.ops[0]);
+      break;
+    case "GET":
+      const allLists = await db.collection("Lists").find({}).toArray();
+      res.json({ status: 200, data: allLists });
+      break;
+  }
+}
---
+++ pages/viewLists/index.js
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import styles from "../../styles/CreateList.module.css";
+
+export async function getServerSideProps(context) {
+  let res = await fetch(`${process.env.SERVER_URL}/api/lists`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  let allLists = await res.json();
+
+  return {
+    props: { allLists },
+  };
+}
+
+export default function CreateList({ allLists }) {
+  return (
+    <main className={styles.main}>
+      <div className={styles.divTitle}>
+        <Link className={styles.arrowBack} href="/">
+          &larr;
+        </Link>
+        <h1 className={styles.title}>Listas</h1>
+      </div>
+
+      <div className={styles.grid}>
+        <a className={styles.card}>
+          <ul>
+            {allLists.data[0].items.map((i) => (
+              <li key={`item+${i}`}>{i}</li>
+            ))}
+          </ul>
+        </a>
+      </div>
+    </main>
+  );
+}

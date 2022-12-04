66d687f31f0fd7e15fa5f61ed239fcc9c2290956
Joao Braganca <joao.braganca@deus.ai>
2022-12-04 18:26:15 +0000

    test NEXT PUBLIC BASE URL

--- pages/viewLists/index.js
+++ pages/viewLists/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import styles from "../../styles/CreateList.module.css";
 
 export async function getServerSideProps(context) {
-  let res = await fetch(`https://${VERCEL_URL}/api/lists`, {
+  let res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/lists`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
